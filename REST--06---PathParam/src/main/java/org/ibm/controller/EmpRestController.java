8d0f8969562e480e142d583c27bf46590f54c67a
GANAPATI BABA MORRYA <GANAPATI BABA MORRYA@192.168.0.14>
2020-02-09 20:38:56 +0530

    10th

--- REST--06---PathParam/src/main/java/org/ibm/controller/EmpRestController.java
+++ REST--06---PathParam/src/main/java/org/ibm/controller/EmpRestController.java
@@ -16,7 +16,7 @@ public class EmpRestController {
     public String showData(
     												@BeanParam Emp emp
     												) {
-		System.out.println("------9 Commit------");
+		System.out.println("------10 Commit------");
 		
 //    	        return emp.toString();
     	        return new StringBuffer()
