d0e2539d208e853c66de807ae3ebf728b233b9ac
GANAPATI BABA MORRYA <GANAPATI BABA MORRYA@192.168.0.14>
2020-02-09 22:32:50 +0530

    1st COMMIT !!!

---
+++ REST--05---TABLE_And_BOX/src/main/java/org/ibm/controller/GetTableRestController.java
@@ -0,0 +1,26 @@
+package org.ibm.controller;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("convert")
+public class GetTableRestController {
+
+    @POST
+    public String showData(
+    												@FormParam ("box")int box
+    												) {
+    	String msg=null;
+    	
+    	int a=Math.round(box/4);
+    	int b=Math.round((box/4)+1);
+    	
+    	if(box%4==0)
+    		msg="TOTAL  TABLE  REQUIRED  IS ::   "+a;
+    	else
+    		msg="TOTAL  TABLE  REQUIRED  IS ::  "+b;
+        return msg;
+    }
+}
+												
---
+++ REST--05---TABLE_And_BOX/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<body style="background-color: cyan">
+    <h2>Count TABLE_And_BOX Application !!!</h2>
+    
+<form action="rest/convert" method="post">
+
+<pre>
+				ENTER THE X no OF BOXES  ::     <input type="text" name="box"/>
+
+				<input type="submit" value="REQUIRED TABLE IS">
+</pre>
+</form>
+
+
+
+
+</body>
